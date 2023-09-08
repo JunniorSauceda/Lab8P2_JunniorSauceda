e7672cf48868d22fcf558e80126a4f574972ff82
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-08 14:13:59 -0600

    Creacion de la clase adminNadador

---
+++ src/Paquete/adminNadador.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Junnior Sauceda
+ */
+public class adminNadador {
+    private ArrayList<Nadador> nadadores= new ArrayList<>();
+    private File Archivo;
+
+    public adminNadador(String path) {
+        this.Archivo = new File(path);
+    }
+
+    public ArrayList<Nadador> getNadadores() {
+        return nadadores;
+    }
+
+    public void setNadadores(ArrayList<Nadador> nadadores) {
+        this.nadadores = nadadores;
+    }
+
+    public File getArchivo() {
+        return Archivo;
+    }
+
+    public void setArchivo(File Archivo) {
+        this.Archivo = Archivo;
+    }
+
+    @Override
+    public String toString() {
+        return "nadadores=" + nadadores ;
+    }
+    public void cargarArchivo() {
+        try {            
+            nadadores = new ArrayList();
+            Nadador temp;
+            if (Archivo.exists()) {
+                FileInputStream entrada
+                    = new FileInputStream(Archivo);
+                ObjectInputStream objeto
+                    = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Nadador) objeto.readObject()) != null) {
+                        nadadores.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del Archivo
+                }
+                objeto.close();
+                entrada.close();
+            }            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(Archivo);
+            bw = new ObjectOutputStream(fw);
+            for (Nadador t : nadadores) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
+}
