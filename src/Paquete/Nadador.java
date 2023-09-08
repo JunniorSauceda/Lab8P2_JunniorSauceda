1eed7089370ace8e15a477237df9510e93f43315
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-08 13:47:37 -0600

    Creacion de la clase Nadador

--- src/Paquete/Nadador.java
+++ src/Paquete/Nadador.java
@@ -9,5 +9,93 @@ package Paquete;
  * @author Junnior Sauceda
  */
 public class Nadador {
+   private String nombre;
+   private Pais nacionalidad;
+   private int edad, distancia,medallas;
+   private double estatura,tiempo;
+   private String estilo;
+
+    public Nadador() {
+    }
+
+    public Nadador(String nombre, Pais nacionalidad, int edad, int distancia, int medallas, double estatura, double tiempo, String estilo) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.edad = edad;
+        this.distancia = distancia;
+        this.medallas = medallas;
+        this.estatura = estatura;
+        this.tiempo = tiempo;
+        this.estilo = estilo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(Pais nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public int getMedallas() {
+        return medallas;
+    }
+
+    public void setMedallas(int medallas) {
+        this.medallas = medallas;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + distancia + 'm';
+    }
    
 }
