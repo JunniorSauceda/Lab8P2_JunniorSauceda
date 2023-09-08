24a75f25a68e7df83b5ca9bdd106973ea9e884b9
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-09-08 13:50:51 -0600

    Creacion de la clase Evento

---
+++ src/Paquete/Evento.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete;
+
+/**
+ *
+ * @author Junnior Sauceda
+ */
+public class Evento {
+    private String Estilo;
+    private int distancia;
+    private double record;
+
+    public Evento() {
+    }
+
+    public Evento(String Estilo, int distancia, double record) {
+        this.Estilo = Estilo;
+        this.distancia = distancia;
+        this.record = record;
+    }
+
+    public String getEstilo() {
+        return Estilo;
+    }
+
+    public void setEstilo(String Estilo) {
+        this.Estilo = Estilo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getRecord() {
+        return record;
+    }
+
+    public void setRecord(double record) {
+        this.record = record;
+    }
+
+    @Override
+    public String toString() {
+        return Estilo + "|" + distancia + "m, Record:" + record + "s";
+    }
+    
+}
